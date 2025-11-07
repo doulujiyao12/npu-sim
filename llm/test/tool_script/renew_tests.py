6174975babdebddf741d517bda51878b36d9f6b3
doulujiyao <2976303243@qq.com>
2025-11-07 22:36:26 +0800

    test: batch test

--- llm/test/tool_script/renew_tests.py
+++ llm/test/tool_script/renew_tests.py
@@ -1,38 +1,41 @@
 import subprocess, glob, os
 from collections import deque
 
-workloads = glob.glob("../llm/test/workload_config/paper/fig9/untested/*.json")
+workloads = glob.glob("../llm/test/workload_config/paper/fig10/tp4/*.json")
 hardware = glob.glob("../llm/test/hardware_config/default_hw.json")
-sim_cfg = "../llm/test/simulation_config/default_spec.json"
+sim_cfg = glob.glob("../llm/test/simulation_config/default_spec.json")
+map_cfg = glob.glob("../llm/test/mapping_config/paper/fig10/tp4/*.spec")
 
 output_file = "result_summary.txt"
 
 with open(output_file, "w") as fout:
     for wl in workloads:
         for hw in hardware:
-            print(f"Running {wl}  x  {hw}")
+            for mmp in map_cfg:
+                for sim in sim_cfg:
+                    print(f"Running {wl}  x  {hw}  x  {mmp}  x  {sim}")
 
-            # 存最后三行
-            last_lines = deque(maxlen=3)
+                    # 存最后三行
+                    last_lines = deque(maxlen=3)
 
-            # 流式读取输出，不缓存全部内容
-            p = subprocess.Popen(
-                ["./npusim", "--workload-config", wl, "--hardware-config", hw, "--simulation-config", sim_cfg],
-                stdout=subprocess.PIPE,
-                stderr=subprocess.STDOUT,
-                text=True,
-                bufsize=1 # 行缓冲
-            )
+                    # 流式读取输出，不缓存全部内容
+                    p = subprocess.Popen(
+                        ["./npusim", "--workload-config", wl, "--hardware-config", hw, "--simulation-config", sim, "--mapping-config", mmp],
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.STDOUT,
+                        text=True,
+                        bufsize=1 # 行缓冲
+                    )
 
-            for line in p.stdout:
-                print(line, end="")   # 正常打印出来，不影响npusim输出体验
-                last_lines.append(line.strip())
+                    for line in p.stdout:
+                        print(line, end="")   # 正常打印出来，不影响npusim输出体验
+                        last_lines.append(line.strip())
 
-            p.wait()
+                    p.wait()
 
-            if len(last_lines) >= 3:
-                useful = list(last_lines)[-3]
-            else:
-                useful = list(last_lines)[-1]
+                    if len(last_lines) >= 3:
+                        useful = list(last_lines)[-3]
+                    else:
+                        useful = list(last_lines)[-1]
 
-            fout.write(f"{os.path.basename(wl)}, {os.path.basename(hw)}, {useful}\n")
+                    fout.write(f"{os.path.basename(wl)}, {os.path.basename(hw)}, {useful}\n")
